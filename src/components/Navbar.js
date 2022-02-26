581ed95acdcfb7fc2cd15922089e89d1bdda7343
Manuel Gonzalez Mouriño <manuelgonzalez4a@gmail.com>
2022-02-26 14:03:47 -0300

    Sitename Change

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -10,7 +10,7 @@ const Navbar = () => {
                 </button>
                 <div className="offcanvas offcanvas-start" tabindex="-1" id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel">
                     <div className="offcanvas-header">
-                        <h5 className="offcanvas-title" id="offcanvasNavbarLabel">Menú</h5>
+                        <h5 className="offcanvas-title" id="offcanvasNavbarLabel">YourVideos</h5>
                         <button type="button" className="btn-close text-reset" data-bs-dismiss="offcanvas" aria-label="Close"></button>
                     </div>
                     <div className="offcanvas-body">
