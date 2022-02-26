af3d77f37c8994f69d2c113d64ee2de1b668dc7a
Manuel Gonzalez Mouriño <manuelgonzalez4a@gmail.com>
2022-02-26 16:16:07 -0300

    Mapping and grid display

--- src/components/DisplayGrid.js
+++ src/components/DisplayGrid.js
@@ -1,9 +1,17 @@
 import React from 'react'
+import DisplayItem from './DisplayItem'
 
-const DisplayGrid = () => {
-  return (
-    <div>DisplayGrid</div>
-  )
+const DisplayGrid = ({ videos }) => {
+    return (
+        <div className="row">
+            {videos.map(vid => 
+                <div className="col-3" key={vid.id}>
+                    <DisplayItem video={vid} />
+                </div>
+            )}
+            
+        </div>
+    )
 }
 
 export default DisplayGrid
