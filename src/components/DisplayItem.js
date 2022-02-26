72f0cd79804def622a49f54da4300bbf6e94e544
Manuel Gonzalez Mouriño <manuelgonzalez4a@gmail.com>
2022-02-26 17:00:44 -0300

    Card changes

--- src/components/DisplayItem.js
+++ src/components/DisplayItem.js
@@ -1,24 +1,27 @@
 import React from 'react'
 import moment from 'moment'
+import { Link } from 'react-router-dom'
 
 const DisplayItem = ({ video }) => {
     const timeAgo = moment(video.subido).fromNow()
 
     return (
-        <div class="card my-3">
-            <img src={`${process.env.PUBLIC_URL}/assets/images/${video.imagen}`} class="card-img-top" alt="..." />
-            <div class="card-body">
-                <h5 class="card-title">
-                    {video.nombre.substr(0, 35)}
-                    {video.nombre.length>35 && <>...</>}
-                </h5>
-                <p class="card-text">{video.autor}</p>
-                <p class="card-text">{video.vistas} visualizaciones</p>
+        <Link to="/search/charlie">
+            <div class="card my-3">
+                <img src={`${process.env.PUBLIC_URL}/assets/images/${video.imagen}`} class="card-img-top" alt="..." />
+                <div class="card-body">
+                    <h5 class="card-title">
+                        {video.nombre.substr(0, 35)}
+                        {video.nombre.length>35 && <>...</>}
+                    </h5>
+                    <p class="card-text">{video.autor}</p>
+                    <p class="card-text">{video.vistas} visualizaciones</p>
+                </div>
+                <div class="card-footer">
+                    <small class="text-muted">{timeAgo}</small>
+                </div>
             </div>
-            <div class="card-footer">
-                <small class="text-muted">{timeAgo}</small>
-            </div>
-        </div>
+        </Link>
     )
 }
 
