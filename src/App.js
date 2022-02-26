87d91a79ba167ed3baf8be0d490e2104af9a2010
Manuel Gonzalez Mouriño <manuelgonzalez4a@gmail.com>
2022-02-26 03:52:16 -0300

    Routes Setup

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import './App.css';
 import Navbar from './components/Navbar';
+import { Routes, Route } from 'react-router-dom'
 
 function App() {
   return (
     <>
       <Navbar />
+      <Routes>
+        <Route path="/" element={<Navbar />} />
+      </Routes>
     </>
   );
 }
