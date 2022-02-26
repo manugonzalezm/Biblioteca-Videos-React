c8439fd8bf099d6db72bfa567bf57519e99a142a
Manuel Gonzalez Mouriño <manuelgonzalez4a@gmail.com>
2022-02-26 16:47:04 -0300

    Bootstrap card in Display Item with props

--- src/components/DisplayItem.js
+++ src/components/DisplayItem.js
@@ -1,9 +1,25 @@
 import React from 'react'
+import moment from 'moment'
 
-const DisplayItem = () => {
-  return (
-    <div>DisplayItem</div>
-  )
+const DisplayItem = ({ video }) => {
+    const timeAgo = moment(video.subido).fromNow()
+
+    return (
+        <div class="card my-3">
+            <img src={`${process.env.PUBLIC_URL}/assets/images/${video.imagen}`} class="card-img-top" alt="..." />
+            <div class="card-body">
+                <h5 class="card-title">
+                    {video.nombre.substr(0, 35)}
+                    {video.nombre.length>35 && <>...</>}
+                </h5>
+                <p class="card-text">{video.autor}</p>
+                <p class="card-text">{video.vistas} visualizaciones</p>
+            </div>
+            <div class="card-footer">
+                <small class="text-muted">{timeAgo}</small>
+            </div>
+        </div>
+    )
 }
 
 export default DisplayItem
