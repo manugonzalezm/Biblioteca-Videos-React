ea68a84b9bc6212b5f72d0a0aabe46fbbb6bb73f
Manuel Gonzalez Mouriño <manuelgonzalez4a@gmail.com>
2022-02-26 14:13:27 -0300

    Display components creation

---
+++ src/components/DisplayGrid.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const DisplayGrid = () => {
+  return (
+    <div>DisplayGrid</div>
+  )
+}
+
+export default DisplayGrid
---
+++ src/components/DisplayGridContainer.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const DisplayGridContainer = () => {
+  return (
+    <div>DisplayGridContainer</div>
+  )
+}
+
+export default DisplayGridContainer
