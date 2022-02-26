903b8c5a057de9a8564e57c93419c686b4df790c
Manuel Gonzalez Mouriño <manuelgonzalez4a@gmail.com>
2022-02-26 15:54:32 -0300

    Receiving data and settting it into the state

--- src/components/DisplayGridContainer.js
+++ src/components/DisplayGridContainer.js
@@ -1,9 +1,28 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import DisplayGrid from './DisplayGrid'
+import dataVideos from '../data/data'
+import { useParams } from 'react-router-dom'
 
 const DisplayGridContainer = () => {
-  return (
-    <div>DisplayGridContainer</div>
-  )
+    const { search } = useParams();
+    console.log(search)
+
+    const[videos, setVideos] = useState(dataVideos);
+    console.log(videos)
+    
+    useEffect(() => {
+        if(search){
+            const filteredVideos = videos.filter(vid => (vid.nombre.toLowerCase()).includes(search.toLowerCase()) || (vid.autor.toLowerCase()).includes(search.toLowerCase()))
+            setVideos(filteredVideos)
+        }
+    }, [search])
+    
+
+    return (
+        <div className="container">
+            <DisplayGrid videos={videos} />
+        </div>
+    )
 }
 
 export default DisplayGridContainer
