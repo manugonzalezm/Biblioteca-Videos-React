069d6323cfdeb5771125f50388d8cba19cbd5e94
Manuel Gonzalez Mouriño <manuelgonzalez4a@gmail.com>
2022-02-27 00:00:38 -0300

    Loading

--- src/components/DisplayGrid.js
+++ src/components/DisplayGrid.js
@@ -2,7 +2,7 @@ import React, { useState } from 'react'
 import DisplayItem from './DisplayItem'
 import ModalVideo from './ModalVideo'
 
-const DisplayGrid = ({ videos }) => {
+const DisplayGrid = ({ videos, loading }) => {
     const [modal, setModal] = useState(false)
     const [tempData, setTempData] = useState([])
 
@@ -10,23 +10,28 @@ const DisplayGrid = ({ videos }) => {
         let tempData = [src, nombre]
         setTempData(mod => [1, ...tempData])
         return setModal(true)
-
     }
 
     return (
         <>
-            <div className="row row-cols-4 mb-5">
-                {videos.map(vid =>
-                    <div className="col" key={vid.id}>
-                        <DisplayItem video={vid} getData={getData} />
-                    </div>
-                )}
-            </div>
             {
-                modal === true ? <ModalVideo src={tempData[1]} nombre={tempData[2]} hide={() => setModal(false)} /> : ''
+                loading ?
+                    <h2 className='cargando'>Cargando...</h2>
+                :
+                    <>
+                        <div className="row row-cols-4 mb-5">
+                            {videos.map(vid =>
+                                <div className="col" key={vid.id}>
+                                    <DisplayItem video={vid} getData={getData} />
+                                </div>
+                            )}
+                        </div>
+                        {
+                            modal === true ? <ModalVideo src={tempData[1]} nombre={tempData[2]} hide={() => setModal(false)} /> : ''
+                        }
+                    </>
             }
         </>
-
     )
 }
 
